a935d9e192df8c7cc659a313e57313d0dbb17150
Jorge Mauricio Dawbarn <jdawbarn@grupoprominente.com>
2023-03-09 14:08:12 -0300

    se modifico ruta del sw

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    /* '/', */
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -38,7 +38,7 @@ self.addEventListener('activate', e => {
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
                 return caches.delete(key);
-            } 
+            }
         });
     });
 
